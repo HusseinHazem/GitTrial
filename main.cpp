0e839008f6300aa112938f98173e6e388dfeef9d
HusseinHazem <36478570+HusseinHazem@users.noreply.github.com>
2022-02-25 22:15:35 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+    int favorite_num;
+    std::cout << "what is your fav number between 1 and 200:";
+    
+    std::cin >> favorite_num;
+    std::cout << "thats my fav number too \nNo really "<< favorite_num <<" is my fav number too";
+    return 0;
+}
