61d354ab127624f6e2ace38d5687e7d28f4e3626
HusseinHazem <h.hazem101@gmail.com>
2022-02-25 22:22:34 +0100

    changed 100 to 200

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 int main(){
     int favorite_num;
-    std::cout << "what is your fav number between 1 and 200:";
+    std::cout << "what is your fav number between 1 and 100:";
     
     std::cin >> favorite_num;
     std::cout << "thats my fav number too \nNo really "<< favorite_num <<" is my fav number too";
